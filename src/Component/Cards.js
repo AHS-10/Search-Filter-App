490c06d82da2b0aad992cc84e6fc0ad613d34ba0
Waphics <waphics10@gmail.com>
2023-02-25 01:21:53 +0500

    Changes made

--- src/Component/Cards.js
+++ src/Component/Cards.js
@@ -1,25 +1,26 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import "../App.css";
-import axios from "axios";
+// import axios from "axios";
+import data from "../data.json";
 
 function Cards(props) {
-  const [api, setApi] = useState(null);
-  useEffect(() => {
-    axios
-      .get("https://fakestoreapi.com/products")
-      .then((response) => {
-        setApi(response.data);
-        console.log(api);
-      })
-      .catch((error) => {
-        // console.log(error);
-      });
-  });
+  // const [aip, setAip] = useState();
+  // useEffect(() => {
+  //   axios
+  //     .get("https://fakestoreapi.com/products")
+  //     .then((response) => {
+  //       setAip(response.data);
+  //       console.log(aip);
+  //     })
+  //     .catch((error) => {
+  //       // console.log(error);
+  //     });
+  // },[]);
   return (
     <>
       <div className="container">
         <div className="row">
-          {api
+          {data
             .filter((item) => {
               if (props.userSearch === "") {
                 return item;
@@ -31,26 +32,26 @@ function Cards(props) {
                 return item;
               }
             })
-            .map((item) => {
+            .map((item,index) => {
               return (
-                <div className="col-sm-4 p-3">
-                  <div class="card shadow-sm ">
+                <div className="col-sm-4 p-3" key={index}>
+                  <div className="card shadow-sm ">
                     <img
                       alt="Card image cap"
                       className="card-img-top img-fluid pt-4"
                       src={item.image}
                     />
                     <hr />
-                    <div class="card-body ">
+                    <div className="card-body ">
                       <p><b>{item.title}</b></p>
-                      {/* <p class="card-text">
+                      {/* <p className="card-text">
                         {item.description}
                       </p> */}
-                      <div class="d-flex justify-content-between align-items-center py-2">
-                        <small class="text-muted">
+                      <div className="d-flex justify-content-between align-items-center py-2">
+                        <small className="text-muted">
                           Price - <b>${item.price}</b>
                         </small>
-                        <small class="text-muted">
+                        <small className="text-muted">
                           Rating - <b>{item.rating.rate}</b>
                         </small>
                       </div>
